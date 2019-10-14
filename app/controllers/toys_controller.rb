a801b618a2430f785674b8d81b05897d9fce0193
EllieChen-Git <elliechen.etc@gmail.com>
2019-10-14 21:47:44 +1100

    added 'create method' & 'new' view

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -2,12 +2,32 @@ class ToysController < ApplicationController
     before_action :setup_data
 
     def index
+    end
 
+    def create
+        Toy.create(
+            name: params[:toy]["name"],
+            description: params[:toy]["description"],
+            date: params[:toy]["date"],
+            user: params[:toy]["user"]
+        )
+            redirect_to(toys_path)
+    end
+
+    def new
     end
 
     def show
         @toy_id = @toys[params[:id].to_i]
     end
+
+    # def edit
+
+    # end
+
+    # def update
+
+    # end
 
     private
     def setup_data
